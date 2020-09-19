b5ae0dad04538eae71bd7f5ca30067dcde1cc952
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-09-19 14:56:54 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from flask_jsonpify import jsonpify
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+
+lol_stats = pd.read_csv(
+    "./2020_LoL_esports_match_data_from_OraclesElixir_20200916.csv")
+
+
+@app.route('/sample', methods=['GET'])
+def hello_world():
+    lol_stats_sample = lol_stats.head(12)
+    lol_stats_sample = lol_stats_sample.to_json(orient="records")
+    parsed = json.loads(lol_stats_sample)
+    print(parsed[0])
+    return json.dumps(parsed)
+
+
+@app.route('/', methods=['POST'])
+def get_data():
+    request_info = request.get_json()
+    requested_lol_stas = lol_stats.loc[lol_stats["league"]
+                                       == request_info["league"]]
+
+    requested_lol_stas = requested_lol_stas.to_json(orient="records")
+    parsed = json.loads(requested_lol_stas)
+
+    return {"data": parsed}, 200
---
+++ get_lol_data.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+lol_stats = pd.read_csv(
+    "./lol_pro_stats_2020.csv")
+# lol_stats["top_pick"] = ""
+# lol_stats["jg_pick"] = ""
+# lol_stats["mid_pick"] = ""
+# lol_stats["bot_pick"] = ""
+# lol_stats["sup_pick"] = ""
+
+# for i in lol_stats.index:
+#     if lol_stats["position"][i] == "team" and lol_stats["side"][i] == "Blue":
+#         lol_stats["top_pick"][i+1] = lol_stats["champion"][i-10]
+#         lol_stats["jg_pick"][i+1] = lol_stats["champion"][i-9]
+#         lol_stats["mid_pick"][i+1] = lol_stats["champion"][i-8]
+#         lol_stats["bot_pick"][i+1] = lol_stats["champion"][i-7]
+#         lol_stats["sup_pick"][i+1] = lol_stats["champion"][i-6]
+
+#     if lol_stats["position"][i] == "team" and lol_stats["side"][i] == "Red":
+#         lol_stats["top_pick"][i+1] = lol_stats["champion"][i-6]
+#         lol_stats["jg_pick"][i+1] = lol_stats["champion"][i-5]
+#         lol_stats["mid_pick"][i+1] = lol_stats["champion"][i-4]
+#         lol_stats["bot_pick"][i+1] = lol_stats["champion"][i-3]
+#         lol_stats["sup_pick"][i+1] = lol_stats["champion"][i-2]
+
+
+# lol_stats.to_csv("lol_pro_stats_2020.csv")
+# print(data)
